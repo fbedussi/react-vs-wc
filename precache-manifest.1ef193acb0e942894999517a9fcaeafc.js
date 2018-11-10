01d0f51266ca759d5371a2103a4af45851b4d7ef
Francesco Bedussi <francesco@francescobedussi.it>
2018-11-10 23:30:21 +0100

    Updates

---
+++ precache-manifest.1ef193acb0e942894999517a9fcaeafc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c55e9407c05902c9f79",
+    "url": "/react-vs-wc/static/js/runtime~main.4c55e940.js"
+  },
+  {
+    "revision": "2c167b80bd86503c93a3",
+    "url": "/react-vs-wc/static/js/main.2c167b80.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/react-vs-wc/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "2c167b80bd86503c93a3",
+    "url": "/react-vs-wc/static/css/main.b40b8cfb.chunk.css"
+  },
+  {
+    "revision": "2621c32e5ca0b4f0d65abaf7cc07ef4a",
+    "url": "/react-vs-wc/index.html"
+  }
+];
