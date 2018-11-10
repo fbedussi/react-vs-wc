121dd39082569a5e388876e89f19a47cc1ecca6e
Francesco Bedussi <francesco@francescobedussi.it>
2018-11-10 23:22:52 +0100

    react build

---
+++ build/precache-manifest.2cc3b5dc09a117063ee927119c76d772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "957884b11d5b61c15010",
+    "url": "/static/js/main.957884b1.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "957884b11d5b61c15010",
+    "url": "/static/css/main.b40b8cfb.chunk.css"
+  },
+  {
+    "revision": "fbd2f099f92ba67d96bb13ef8c548aae",
+    "url": "/index.html"
+  }
+];
