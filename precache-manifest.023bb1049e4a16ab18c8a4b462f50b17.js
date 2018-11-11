203e6edea16f39242300a576d93cb2a1fadee9d6
Francesco Bedussi <francesco@francescobedussi.it>
2018-11-11 18:30:37 +0100

    Updates

--- precache-manifest.023bb1049e4a16ab18c8a4b462f50b17.js
+++ precache-manifest.023bb1049e4a16ab18c8a4b462f50b17.js
@@ -4,19 +4,15 @@ self.__precacheManifest = [
     "url": "/react-vs-wc/static/js/runtime~main.4c55e940.js"
   },
   {
-    "revision": "3b5121cbb9a7b9d10db5",
-    "url": "/react-vs-wc/static/js/main.3b5121cb.chunk.js"
+    "revision": "1e0004ce731230cbf344",
+    "url": "/react-vs-wc/static/js/main.1e0004ce.chunk.js"
   },
   {
     "revision": "3da870a3c1c4b41662f2",
     "url": "/react-vs-wc/static/js/1.3da870a3.chunk.js"
   },
   {
-    "revision": "3b5121cbb9a7b9d10db5",
-    "url": "/react-vs-wc/static/css/main.b40b8cfb.chunk.css"
-  },
-  {
-    "revision": "e660d0fc662b55c1c0d2c00ee0920ca4",
+    "revision": "fe6e171d7d0665cb84c07c7df9c31675",
     "url": "/react-vs-wc/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-vs-wc/precache-manifest.cfb7be6f2be89381733bd4095ef905ac.js"
+  "/react-vs-wc/precache-manifest.023bb1049e4a16ab18c8a4b462f50b17.js"
 );
 
 workbox.clientsClaim();
